7dbb3f2ee8d0ad04cf8ecf588fea4468b5badc45
haohaohaodianhao <haohaohaodianhao@gmail.com>
2024-12-03 03:11:04 +0000

    提交代码

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-node hello.js
+# 安装依赖
+cd /home/devbox/project/file-manage-sys-backend
+npm install
+
+# 启动后端服务
+node app.js &
+
+# 安装前端依赖并构建
+cd /home/devbox/project
+npm install
+npm run build
+
+# 使用 serve 启动前端构建结果
+npm install -g serve
+serve -s dist -l 8080
